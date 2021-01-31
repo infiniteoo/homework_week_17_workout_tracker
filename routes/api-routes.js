01dd2c76c05f50e01ac8a3df86d6171f431ac59b
Troy Dorman <troydorman@gmail.com>
2021-01-30 18:56:03 -0700

    getting the html routes set up.  the api routes are proving to be a challenge early on..

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+router.put('/api/workouts', function (req, res, next) {
+  console.log('req, res, next', req, res, next)
 })
 
 module.exports = router
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,15 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Workout Tracker' })
+})
+
+router.get('/stats', function (req, res, next) {
+  res.render('stats', { title: 'Stats' })
+})
+
+router.get('/exercise', function (req, res, next) {
+  res.render('exercise', { title: 'Stats' })
 })
 
 module.exports = router
