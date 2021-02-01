3e4f9887d89fbd1e0a906b640b6574d1e2bda997
Troy Dorman <troydorman@gmail.com>
2021-02-01 10:35:31 -0700

    got "day" variable added to the add exercise route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -7,9 +7,11 @@ router.get('/workouts', function (req, res, next) {
   console.log('/workouts get route touched')
 })
 
+// need to write to the 'day' variable by adding a Date()
 router.put('/workouts/:id', function (req, res) {
   console.log('weve touched the /api/workouts/:id route: ', req.body)
-  db.collection('workouts').insert(req.body, results => {
+  const finalObjectToInsert = { day: new Date().setDate(new Date().getDate()), ...req.body }
+  db.collection('workouts').insertOne(finalObjectToInsert, results => {
     console.log('value of results: ', results)
     res.json(req.body)
   })
