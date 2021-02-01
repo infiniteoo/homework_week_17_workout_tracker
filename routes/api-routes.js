47703ad0e4839cf716fea9de77378b19039a8eb6
Troy Dorman <troydorman@gmail.com>
2021-02-01 10:42:48 -0700

    "add exercise" wasnt working just right.. its now fixed. i swear!

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,7 +10,7 @@ router.get('/workouts', function (req, res, next) {
 // need to write to the 'day' variable by adding a Date()
 router.put('/workouts/:id', function (req, res) {
   console.log('weve touched the /api/workouts/:id route: ', req.body)
-  const finalObjectToInsert = { day: new Date().setDate(new Date().getDate()), ...req.body }
+  const finalObjectToInsert = { day: new Date().setDate(new Date().getDate()), exercises: [req.body] }
   db.collection('workouts').insertOne(finalObjectToInsert, results => {
     console.log('value of results: ', results)
     res.json(req.body)
