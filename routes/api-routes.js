d1077dd40f91a367bed8ba4221d23f5a99c3d3ad
Troy Dorman <troydorman@gmail.com>
2021-02-01 17:10:43 -0700

    omfg.. weve got pie charts and graphs being populated! SOO COOL!

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,19 +16,43 @@ router.get('/workouts', function (req, res, next) {
   })
 })
 
+router.get('/workouts/range', function (req, res) {
+  console.log('/api/workouts/range touched!')
+  db.collection('workouts').find({}).toArray((err, items) => {
+    if (err) {
+      throw err
+    } else {
+      console.log(items)
+      res.json(items)
+    }
+  })
+})
+
 // instead of writing a new entry into the database, this should actually add a new exercise to the array of the supplied id in the req.params
 router.put('/workouts/:id', function (req, res) {
   console.log('the id of the param is:', req.params.id)
   console.log('weve touched the PUT /api/workouts/:id route:', req.body)
 
-  db.collection('workouts').updateOne(
-    { _id: ObjectId(req.params.id) },
-    {
-      $push: {
-        exercises: req.body
-      }
+  if (req.params.id === 'undefined') {
+    const finalObjectToInsert = {
+      day: new Date().setDate(new Date().getDate()),
+      exercises: [req.body]
     }
-  )
+    db.collection('workouts').insertOne(finalObjectToInsert, results => {
+      console.log('value of results: ', results)
+      res.json(req.body)
+    })
+  } else {
+    db.collection('workouts').updateOne(
+      { _id: ObjectId(req.params.id) },
+      {
+        $push: {
+          exercises: req.body
+        }
+      }
+    )
+    res.json(req.body)
+  }
 })
 
 // need to create a post route for /api/workouts that creates a whole new workout
